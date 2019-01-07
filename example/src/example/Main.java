b76d89c71ba6f4b60690165b5a85e47e689032b8
akhilekkaldevi <akhil.e@kustommade.com>
2019-01-07 18:11:37 +0530

    example

--- example/src/example/Main.java
+++ example/src/example/Main.java
@@ -4,6 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Rectangle r1 = new Rectangle(0, 0, 3, 3);
+		Rectangle r2 = new Rectangle(2, 0, 4, 4);
+		Rectangle r3 = new Rectangle(5, 5, 10, 10);
+		Rectangle.collide(r2, r3);
+
+		// TODO Auto-generated method stub
 
 	}
 
--- example/src/example/Rectangle.java
+++ example/src/example/Rectangle.java
@@ -13,30 +13,17 @@ public class Rectangle {
 	}
 
 	static void collide(Rectangle r1, Rectangle r2) {
-		boolean b = false;
-		if (r1.ax1 < r2.ax1) {
-			if (r2.ax1 < r1.cx1 || r2.ay1 < r1.cy1)
-				b = true;
-		} else if (r2.ax1 < r1.ax1) {
-			if (r1.ax1 < r2.cx1 || r1.ay1 < r2.cy1)
-				b = true;
-
-		}
+		boolean b = true;
+		if (r1.ax1 > r2.cx1 || r2.ax1 > r1.cx1)
+			b = false;
+		if (r1.ay1 < r2.cy1 || r2.ay1 < r1.cy1)
+			b = false;
 
 		if (b) {
 			int a[] = { Math.max(r1.ax1, r2.ax1), Math.max(r1.ay1, r2.ay1), Math.min(r1.cx1, r2.cx1),
 					Math.min(r1.cy1, r2.cy1) };
 			System.out.println(Arrays.toString(a));
 		}
-	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Rectangle r1 = new Rectangle(0, 0, 3, 3);
-		Rectangle r2 = new Rectangle(2, 0, 4, 4);
-		Rectangle r3 = new Rectangle(5, 5, 10, 10);
-		collide(r1, r2);
-
 	}
 
 }
